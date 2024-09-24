4ce707abc01726aa6bc1d8e3775aee08674d700f
CodeBear <48406534+yXiangXiong@users.noreply.github.com>
2024-09-24 22:32:05 +0800

    Update run_swin.sh

--- VariMix/run_swin.sh
+++ VariMix/run_swin.sh
@@ -29,19 +29,6 @@ python train.py --cuda --train_dir /home/xiangyu/data/breast-ultrasound-dataset/
 --save_dirname swin_v2_t_5 --dataset_name breast-ultrasound-dataset --num_classes 3 --num_domains 3 \
 --model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 20000
 
-python train.py --cuda --train_dir /home/xiangyu/data/breast-ultrasound-dataset/train_dir \
---valid_dir /home/xiangyu/data/breast-ultrasound-dataset/valid_dir \
---json_path /home/xiangyu/data/breast-ultrasound-dataset/cat_to_name.json \
---save_dirname swin_v2_t_6 --dataset_name breast-ultrasound-dataset --num_classes 3 --num_domains 3 \
---model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 20000
-
-python train.py --cuda --train_dir /home/xiangyu/data/breast-ultrasound-dataset/train_dir \
---valid_dir /home/xiangyu/data/breast-ultrasound-dataset/valid_dir \
---json_path /home/xiangyu/data/breast-ultrasound-dataset/cat_to_name.json \
---save_dirname swin_v2_t_7 --dataset_name breast-ultrasound-dataset --num_classes 3 --num_domains 3 \
---model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 20000
-
-
 
 python train.py --cuda --train_dir /home/xiangyu/data/covid19-pneumonia-dataset/train_dir \
 --valid_dir /home/xiangyu/data/covid19-pneumonia-dataset/valid_dir \
@@ -71,18 +58,6 @@ python train.py --cuda --train_dir /home/xiangyu/data/covid19-pneumonia-dataset/
 --valid_dir /home/xiangyu/data/covid19-pneumonia-dataset/valid_dir \
 --json_path /home/xiangyu/data/covid19-pneumonia-dataset/cat_to_name.json \
 --save_dirname swin_v2_t_5 --dataset_name covid19-pneumonia-dataset --num_classes 3 --num_domains 3 \
---model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 50000
-
-python train.py --cuda --train_dir /home/xiangyu/data/covid19-pneumonia-dataset/train_dir \
---valid_dir /home/xiangyu/data/covid19-pneumonia-dataset/valid_dir \
---json_path /home/xiangyu/data/covid19-pneumonia-dataset/cat_to_name.json \
---save_dirname swin_v2_t_6 --dataset_name covid19-pneumonia-dataset --num_classes 3 --num_domains 3 \
---model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 50000
-
-python train.py --cuda --train_dir /home/xiangyu/data/covid19-pneumonia-dataset/train_dir \
---valid_dir /home/xiangyu/data/covid19-pneumonia-dataset/valid_dir \
---json_path /home/xiangyu/data/covid19-pneumonia-dataset/cat_to_name.json \
---save_dirname swin_v2_t_7 --dataset_name covid19-pneumonia-dataset --num_classes 3 --num_domains 3 \
 --model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 50000
 
 
@@ -117,18 +92,6 @@ python train.py --cuda --train_dir /home/xiangyu/data/eye-disease-dataset/train_
 --save_dirname swin_v2_t_5 --dataset_name eye-disease-dataset --num_classes 4 --num_domains 4 \
 --model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 50000
 
-python train.py --cuda --train_dir /home/xiangyu/data/eye-disease-dataset/train_dir \
---valid_dir /home/xiangyu/data/eye-disease-dataset/valid_dir \
---json_path /home/xiangyu/data/eye-disease-dataset/cat_to_name.json \
---save_dirname swin_v2_t_6 --dataset_name eye-disease-dataset --num_classes 4 --num_domains 4 \
---model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 50000
-
-python train.py --cuda --train_dir /home/xiangyu/data/eye-disease-dataset/train_dir \
---valid_dir /home/xiangyu/data/eye-disease-dataset/valid_dir \
---json_path /home/xiangyu/data/eye-disease-dataset/cat_to_name.json \
---save_dirname swin_v2_t_7 --dataset_name eye-disease-dataset --num_classes 4 --num_domains 4 \
---model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 50000
-
 
 
 python train.py --cuda --train_dir /home/xiangyu/data/fetal-ultrasound-dataset/train_dir \
@@ -159,16 +122,4 @@ python train.py --cuda --train_dir /home/xiangyu/data/fetal-ultrasound-dataset/t
 --valid_dir /home/xiangyu/data/fetal-ultrasound-dataset/valid_dir \
 --json_path /home/xiangyu/data/fetal-ultrasound-dataset/cat_to_name.json \
 --save_dirname swin_v2_t_5 --dataset_name fetal-ultrasound-dataset --num_classes 6 --num_domains 6 \
---model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 25000 --n_epochs 50 --n_epochs_decay 50
-
-python train.py --cuda --train_dir /home/xiangyu/data/fetal-ultrasound-dataset/train_dir \
---valid_dir /home/xiangyu/data/fetal-ultrasound-dataset/valid_dir \
---json_path /home/xiangyu/data/fetal-ultrasound-dataset/cat_to_name.json \
---save_dirname swin_v2_t_6 --dataset_name fetal-ultrasound-dataset --num_classes 6 --num_domains 6 \
---model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 25000 --n_epochs 50 --n_epochs_decay 50
-
-python train.py --cuda --train_dir /home/xiangyu/data/fetal-ultrasound-dataset/train_dir \
---valid_dir /home/xiangyu/data/fetal-ultrasound-dataset/valid_dir \
---json_path /home/xiangyu/data/fetal-ultrasound-dataset/cat_to_name.json \
---save_dirname swin_v2_t_7 --dataset_name fetal-ultrasound-dataset --num_classes 6 --num_domains 6 \
 --model_name swin_v2_t --train_batchSize 128 --val_batchSize 128 --resume_iter 25000 --n_epochs 50 --n_epochs_decay 50
